f1f18e2171663691e87295778a57a2b83da83f97
JBailyes <58641432+JBailyes@users.noreply.github.com>
2020-12-10 08:22:57 +0000

    Day 10 star 1

--- 2020/10-1.py
+++ 2020/10-1.py
@@ -5,7 +5,7 @@ import os
 def main():
     day = os.path.basename(__file__).split('-')[0]
     challenge_input = '{}-input.txt'.format(day)
-    challenge_input = '{}-example.txt'.format(day)
+    # challenge_input = '{}-example.txt'.format(day)
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', default=challenge_input)
@@ -19,6 +19,21 @@ def main():
     numbers = []
     for line in lines:
         numbers.append(int(line))
+
+    sorted_numbers = sorted(numbers)
+    sorted_numbers.append(sorted_numbers[-1] + 3)
+    differences = {}
+    output = 0
+    for number in sorted_numbers:
+        difference = number - output
+        if difference not in differences.keys():
+            differences[difference] = 0
+        differences[difference] += 1
+        print('{} -> {} = {}'.format(output, number, difference))
+        output = number
+
+    print(differences)
+    print(differences[1] * differences[3])
 
 
 if __name__ == "__main__":
