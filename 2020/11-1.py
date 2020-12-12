9d452d44889f61e2eade2d9cf3163c0a979b186a
JBailyes <58641432+JBailyes@users.noreply.github.com>
2020-12-12 16:13:29 +0000

    11-1 done

--- 2020/11-1.py
+++ 2020/11-1.py
@@ -5,8 +5,7 @@ import os
 def main():
     day = os.path.basename(__file__).split('-')[0]
     challenge_input = '{}-input.txt'.format(day)
-    # challenge_input = '{}-example-1.txt'.format(day)
-    # challenge_input = '{}-example-2.txt'.format(day)
+    # challenge_input = '{}-example.txt'.format(day)
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', default=challenge_input)
@@ -17,32 +16,28 @@ def main():
         for line in infile:
             lines.append(line.strip())
 
+    floorplan = Floorplan(lines)
+    previously_occupied_positions = set()
 
-class Floorplan:
-    def __init__(self, data: list[str]):
-        self.plan = {}
-        for x in range(len(data[0])):
-            for y in range(len(data)):
-                if data[y][x] == 'L':
-                    self.add_seat(x, y)
+    while True:
+        new_occupied_positions = set()
+        for pos in floorplan.seat_positions:
+            occupied_adjacents = len(floorplan.get_adjacent_seats(pos) & previously_occupied_positions)
+            if pos not in previously_occupied_positions and occupied_adjacents == 0:
+                new_occupied_positions.add(pos)
+            elif pos in previously_occupied_positions and occupied_adjacents < 4:
+                new_occupied_positions.add(pos)
+        if new_occupied_positions == previously_occupied_positions:
+            print('Stabilised seat occupations:', len(new_occupied_positions))
+            break
+        previously_occupied_positions = new_occupied_positions
 
-    def add_seat(self, x: int, y: int):
-        pos = Pos(x, y)
-        self.plan[pos] = Seat(pos)
-
-    def analyse_layout(self):
-        for seat in self.plan:
-            for x in range(seat.x - 1, seat.x + 1):
-                for y in range(seat.y - 1, seat.y + 1):
-                    pos = Pos(x, y)
-                    if pos != seat.pos and pos in self.plan.keys():
-                        seat.add_adjacent(self.plan[pos])
-        
 
 class Pos:
     def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
+        self._hash = y * 100 + x
 
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
@@ -50,15 +45,30 @@ class Pos:
     def __repr__(self):
         return '({},{})'.format(self.x, self.y)
 
+    def __hash__(self):
+        return self._hash
 
-class Seat:
-    def __init__(self, pos: Pos):
-        self.occupied: bool = False
-        self.position = pos
-        self.adjacent_seats = set()
 
-    def add_adjacent(self, seat):
-        self.adjacent_seats.add(seat)
+class Floorplan:
+    def __init__(self, data: list[str]):
+        self.seat_positions: set[Pos] = set()
+        for x in range(len(data[0])):
+            for y in range(len(data)):
+                if data[y][x] == 'L':
+                    self.seat_positions.add(Pos(x, y))
+
+        self.adjacents = {}
+        for seat_pos in self.seat_positions:
+            seat_adjacents = set()
+            for x in [seat_pos.x - 1, seat_pos.x, seat_pos.x + 1]:
+                for y in [seat_pos.y - 1, seat_pos.y, seat_pos.y + 1]:
+                    adjacent_pos = Pos(x, y)
+                    if adjacent_pos != seat_pos and adjacent_pos in self.seat_positions:
+                        seat_adjacents.add(adjacent_pos)
+            self.adjacents[seat_pos] = seat_adjacents
+
+    def get_adjacent_seats(self, pos):
+        return self.adjacents[pos]
 
 
 if __name__ == "__main__":
