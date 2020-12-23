3350ad768568f7b4cf003556b518aa942540c571
JBailyes <58641432+JBailyes@users.noreply.github.com>
2020-12-23 16:54:52 +0000

    Prep for Christmas Eve

---
+++ 2020/24-1.py
@@ -0,0 +1,10 @@
+from aocutils import load_input
+
+
+def main():
+    puzzle_input = load_input(__file__)
+    puzzle_input = load_input(__file__, 'example')
+
+
+if __name__ == "__main__":
+    main()
