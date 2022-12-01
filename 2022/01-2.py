99f1871425a502cec9c2063b36ea6c6b3acfd7dc
JBailyes <58641432+JBailyes@users.noreply.github.com>
2022-12-01 05:18:26 +0000

    2022 day 1 done

--- 2022/01-2.py
+++ 2022/01-2.py
@@ -10,15 +10,15 @@ def main():
     totals = []
 
     current_total = 0
-    for line in puzzle_input:
+    for line in puzzle_input + ['']:
         if line == '':
             totals.append(current_total)
             current_total = 0
         else:
             current_total += int(line)
     
-    # Correct answer: 69289
-    print(max(totals))
+    # Correct answer: 205615
+    print(sum(sorted(totals, reverse=True)[0:3]))
 
 
 if __name__ == "__main__":
