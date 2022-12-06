c77f8e065d1e076f1de8217a3b935df3e78c6c51
JBailyes <58641432+JBailyes@users.noreply.github.com>
2022-12-06 05:15:22 +0000

    Day 6 done

--- 2022/06-2.py
+++ 2022/06-2.py
@@ -3,18 +3,18 @@ from aocutils import load_input
 
 def main():
     puzzle_input = load_input(__file__)
-    # puzzle_input = ['nppdvjthqldpwncqszvftbrmjlhg']
+    # puzzle_input = ['mjqjpqmgbljsphdztnvjfqwrcgsmlb']
 
     def is_start_of_packet(text):
         return len(set(text)) == len(text)
 
     line = puzzle_input[0]
-    for i in range(0, len(line) - 3):
-        if is_start_of_packet(line[i:i+4]):
-            print(i + 4)
+    for i in range(0, len(line) - 13):
+        if is_start_of_packet(line[i:i+14]):
+            print(i + 14)
             return
 
-    # Correct answer: 1816
+    # Correct answer: 2625
 
 
 if __name__ == "__main__":
